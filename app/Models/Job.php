2c81f10c7d7aba7309cacaa4a244ab988d8c31f6
Taufan Prasetyo <taufanremote@gmail.com>
2024-07-27 17:42:38 +0700

    Eps 11: Eloquent Relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -12,4 +13,9 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
