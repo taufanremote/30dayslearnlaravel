7b3a109e82ee0ab69d684ce54d6026faf9587f93
Taufan Prasetyo <taufanremote@gmail.com>
2024-07-29 01:05:03 +0700

    Eps 19.5: Route Resource

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
