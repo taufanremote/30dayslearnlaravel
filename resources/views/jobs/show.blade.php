6d5e2a13306d143396b9e7245ff007c1ab8d37ba
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-31 06:07:06 +0700

    Eps 21.1: Gate

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,7 +8,9 @@
         This job pays {{ $job['salary'] }} per year.
     </p>
 
-    <p class="mt-6">
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
-    </p>
+    @can('edit-job', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
+        </p>
+    @endcan
 </x-layout>
