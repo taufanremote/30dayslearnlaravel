3dc314c1f03bda76d0754f391dbfe6bbc5d6b7db
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-31 07:09:42 +0700

    Eps 21.3: Policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
-
-        Gate::define('edit-job', function (User $user, Job $job) {
-            return $job->employer->user->is($user);
-        });
     }
 }
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,7 +8,7 @@
         This job pays {{ $job['salary'] }} per year.
     </p>
 
-    @can('edit-job', $job)
+    @can('edit', $job)
         <p class="mt-6">
             <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
         </p>
