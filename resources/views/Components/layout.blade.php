868c2deae9c055bc41e9c1b1ec1b44dfd1942aed
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-31 22:52:45 +0700

    Eps 24: Add TailwindCSS

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 </head>
 
 <body class="h-full">
